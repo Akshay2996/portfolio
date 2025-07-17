d4c688ddaf278aa4d346f92621ca2c6fd08a355a
Akshay Sharma <akshaysharmaay@gmail.com>
2025-07-17 09:59:12 +0530

    Creating the header section

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <div className="fixed top-0 left-1/2 "></div>
+      <nav></nav>
+    </header>
+  );
+}
