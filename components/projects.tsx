0d8b4300c40cd20d9b74fbef7ba45b9e0e94e72a
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-14 01:22:39 +0530

    Setting activeSection to project section when scrolled

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView]);
+
   return (
-    <section id="projects" className="scroll-mt-30">
+    <section ref={ref} id="projects" className="scroll-mt-30">
       <SectionHeading>Past projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
