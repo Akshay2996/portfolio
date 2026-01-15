1d8139e0b08d954beb38998ae6b73aeb74e89b04
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-16 03:24:42 +0530

    Working on contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
@@ -8,6 +10,13 @@ export default function Contact() {
   return (
     <section id="contact" className="mb-28 scroll-mt-30 sm:mb-40" ref={ref}>
       <SectionHeading>Contact me</SectionHeading>
+      <p className="text-gray-700 -mt-6">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:akshaysharmaay@gmail.com">
+          akshaysharmaay@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
     </section>
   );
 }
