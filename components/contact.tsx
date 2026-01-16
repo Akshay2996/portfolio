6c46092e92881a9071cd2147001b5243890a3ba1
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-16 17:49:08 +0530

    Added styling to the contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,12 +3,17 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact", 0.5);
 
   return (
-    <section id="contact" className="mb-28 scroll-mt-30 sm:mb-40" ref={ref}>
+    <section
+      id="contact"
+      className="mb-28 scroll-mt-30 sm:mb-40 w-[min(100%,38rem)] text-center"
+      ref={ref}
+    >
       <SectionHeading>Contact me</SectionHeading>
       <p className="text-gray-700 -mt-6">
         Please contact me directly at{" "}
@@ -17,6 +22,30 @@ export default function Contact() {
         </a>{" "}
         or through this form.
       </p>
+
+      <form className="mt-10 flex flex-col">
+        <input
+          className="h-14 px-4 rounded-lg borderBlack"
+          type="email"
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
+        <button
+          type="submit"
+          className="group cursor-pointer flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-100"
+        >
+          Submit{" "}
+          <FaPaperPlane
+            className="text-xs opacity-75 transition-all
+          group-hover:translate-x-1
+          group-hover:-translate-y-1
+          "
+          />{" "}
+        </button>
+      </form>
     </section>
   );
 }
