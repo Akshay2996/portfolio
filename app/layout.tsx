beb0fec85cbb3e87c7ca1e0ebcd2d8dffaf40f60
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-20 23:27:53 +0530

    Added toaster for alerts when sending email

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,8 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+
+          <Toaster position="top-right" reverseOrder={false} />
         </ActiveSectionContextProvider>
       </body>
     </html>
