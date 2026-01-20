50c091eca70a30e0bcf37078c9fc6f5edb827061
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-20 23:27:21 +0530

    Added next configuration for image qualities

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    qualities: [75, 85, 95, 100],
     remotePatterns: [
       {
         protocol: "https",
