aee48ae0e178bfdd0e9b81c6823f2dd7e7182a08
Akshay Sharma <akshaysharmaay@gmail.com>
2026-01-22 16:16:18 +0530

    Added dark mode styling in Experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -10,9 +10,11 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
+  const { theme } = useTheme();
 
   return (
     <section ref={ref} id="experience" className="mb-28 scroll-mt-30 sm:mb-40">
@@ -22,26 +24,35 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: "#f3f4f6",
+                background:
+                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
-              contentArrowStyle={{ borderRight: "0.4rem solid #9ca3af" }}
+              contentArrowStyle={{
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
+              }}
               date={experience.date}
               icon={experience.icon}
               iconStyle={{
-                background: "white",
+                background:
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                 fontSize: "2rem",
               }}
             >
-              <h2 className="font-semibold capitalize">
+              <h2 className="font-semibold capitalize dark:!text-white">
                 {experience.companyName}
               </h2>
               <h3 className="font-semibold capitalize">{experience.title}</h3>
-              <p className="font-normal !mt-0">{experience.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
+              <p className="font-normal !mt-0 dark:text-white/85">
+                {experience.location}
+              </p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {experience.description}
               </p>
             </VerticalTimelineElement>
